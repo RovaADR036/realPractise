71f4dcd14c7f6d7d3d7c635fb2e1ec836cab1b66
Rovasoa <andrianiainarovasoaherimandimb@gmail.com>
2025-07-25 13:41:22 +0200

    jorus 1 ✔

--- src/components/BouttonFacteur.jsx
+++ src/components/BouttonFacteur.jsx
@@ -7,9 +7,7 @@ function BouttonFacteur() {
         ecouteur de changement dans
         <strong> nombre de changement = {facteur} </strong>
       </p>
-
       <button onClick={() => setFacteur(facteur + 1)}>
-        {" "}
         appuiez est voir le changement
       </button>
     </div>
